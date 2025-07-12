dd223a8e1e17c6300cd296cc381ad9c2de7dcd0a
Fitsum Mesfin <58865020+fitsumM12@users.noreply.github.com>
2025-07-12 09:51:29 +0300

    ✅Updated

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import "perfect-scrollbar/css/perfect-scrollbar.css";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/deep-eye">
     <App />
   </BrowserRouter>
 );
